561080ed09c7971e740e140464290a7325a8245d
Rokas Jeriomenko <rokas@jerio.me>
2024-01-04 01:58:59 -0500

    Add ssh key-pair

--- main.tf
+++ main.tf
@@ -11,19 +11,29 @@ variable "AWS_SECRET_ACCESS_KEY" {
   sensitive = true
 }
 
+output "instance_ip_address" {
+  value = aws_instance.example.public_ip
+}
+
 provider "aws" {
   region = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   assume_role {
     role_arn = var.role_arn
-  }  
+  }
 }
 
 resource "aws_instance" "example" {
   ami = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
+  key_name = aws_key_pair.ssh-key.key_name
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "aws-ssh-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
