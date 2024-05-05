914e574c80eaf59d63fdceab8fd8446030926042
Rokas Jeriomenko <rokas@jerio.me>
2024-05-05 18:59:09 -0400

    add updated charles ami

--- main.tf
+++ main.tf
@@ -11,13 +11,13 @@ variable "AWS_SECRET_ACCESS_KEY" {
   sensitive = true
 }
 
-# output "instance_ssh_address" {
-#   value = "ubuntu@ec2-${replace(aws_instance.projects-ready.public_ip, ".", "-")}.compute-1.amazonaws.com"
-# }
-
-output "second_debug_address" {
-  value = "ubuntu@ec2-${replace(aws_instance.java-ready.public_ip, ".", "-")}.compute-1.amazonaws.com"
+output "instance_ssh_address" {
+  value = "ubuntu@ec2-${replace(aws_instance.projects-ready.public_ip, ".", "-")}.compute-1.amazonaws.com"
 }
+
+# output "second_debug_address" {
+#   value = "ubuntu@ec2-${replace(aws_instance.java-ready.public_ip, ".", "-")}.compute-1.amazonaws.com"
+# }
 
 provider "aws" {
   region = "us-east-1"
@@ -28,25 +28,25 @@ provider "aws" {
   }
 }
 
-# resource "aws_instance" "projects-ready" {
-#   ami = "ami-023aee11add72edef"
-#   instance_type = "t2.medium"
-#   key_name = aws_key_pair.ssh-key.key_name
-#   tags = {
-#     Name = "projects-ready"
-#   }
-#   vpc_security_group_ids = [aws_security_group.ingress-egress-all-test.id]
-# }
-
-resource "aws_instance" "java-ready" {
-  ami = "ami-07d9b9ddc6cd8dd30"
+resource "aws_instance" "projects-ready" {
+  ami = "ami-01d3282e9e0b6f716"
   instance_type = "t2.medium"
   key_name = aws_key_pair.ssh-key.key_name
   tags = {
-    Name = "java-ready"
+    Name = "projects-ready"
   }
   vpc_security_group_ids = [aws_security_group.ingress-egress-all-test.id]
 }
+
+# resource "aws_instance" "java-ready" {
+#   ami = "ami-07d9b9ddc6cd8dd30"
+#   instance_type = "t2.medium"
+#   key_name = aws_key_pair.ssh-key.key_name
+#   tags = {
+#     Name = "java-ready"
+#   }
+#   vpc_security_group_ids = [aws_security_group.ingress-egress-all-test.id]
+# }
 
 resource "aws_key_pair" "ssh-key" {
   key_name = "aws-ssh-key"
